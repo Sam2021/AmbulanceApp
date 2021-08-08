0f3626bbc754b36ccbb7ea83ef964bea7538f7d4
Smit Parikh <parikhsmit786@gmail.com>
2021-08-08 11:52:34 +0530

    first

---
+++ Part 1(Login and Signup with firebase)/README.md
@@ -0,0 +1,20 @@
+# React Native Firebase Login Screen
+
+This repo is proudly sponsored by:
+
+![Logo]()
+ 
+
+
+
+
+```
+
+## Preview
+
+![homescreen](assets/Firstscreen.jpg)
+![login]
+![register]
+![forgot]
+![dashboard]
+
---
+++ Part 1(Login and Signup with firebase)/src/screens/Dashboard.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Paragraph from '../components/Paragraph'
+import Button from '../components/Button'
+import { logoutUser } from '../api/auth-api'
+
+
+export default function Dashboard() {
+  
+  return (
+    
+    <Background>
+      <Logo />
+      <Header>Welcome to LSAS</Header>
+      
+      <Button mode="outlined" onPress={logoutUser}>
+        Logout
+      </Button>
+    </Background>
+  )
+}
